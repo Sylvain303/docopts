2fbe39d1f38e3b866efb09040a6433d671609509
Sylvain Viart <sylvain303@github>
2018-06-19 06:00:50 +0200

    add benchmark scripts

---
+++ bench/timing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# float computation US number format
+export LANG=C
+
+tmpf=/dev/shm/timing.out
+rm -f $tmpf
+START=$(date +%s.%N)
+# do something #######################
+echo "running: $@"
+
+eval "$@" &> $tmpf
+
+#######################################
+END=$(date +%s.%N)
+DIFF=$( echo "scale=3; (${END} - ${START})*1000/1" | bc )
+echo "${DIFF} ms output: $(wc -l $tmpf)"
